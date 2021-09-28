8fffa681fda4b7de95b422edb26b7e725c788b1f
guangyee <gyee@suse.com>
2021-09-27 23:40:08 -0700

    Exit with the proper RC
    setup_harvester.sh should return the RC from ansible-playbook so it
    truly reflects the overall result.

--- vagrant-pxe-harvester/setup_harvester.sh
+++ vagrant-pxe-harvester/setup_harvester.sh
@@ -5,5 +5,6 @@ ROOTDIR=$(dirname $(readlink -e $MYNAME))
 
 pushd $ROOTDIR
 ansible-playbook ansible/setup_harvester.yml --extra-vars "@settings.yml"
+ANSIBLE_PLAYBOOK_RESULT=$?
 popd
-
+exit $ANSIBLE_PLAYBOOK_RESULT
