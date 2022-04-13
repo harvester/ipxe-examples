fb3ab5ef68a7eef76f9a96b425ca7480bc996551
Mike Russell <michael.russell@suse.com>
2022-04-12 20:32:32 -0700

    feat: WIP Air Gapped Single Node Rancher
    * inital commit, scaffolding content and base integration with
    ipxe harvester example
    
    Resolves: feat/2096-air-gapped-rancher-single-node-proof-of-concept

---
+++ vagrant-pxe-harvester/ansible/roles/rancher/files/get-rancher-scripts.sh
@@ -0,0 +1,9 @@
+# vim get-rancher-scripts
+#!/bin/bash
+if [[ $# -eq 0 ]] ; then
+    echo 'This requires you to pass a version for the url like "v2.6.3"'
+    exit 1
+fi
+wget https://github.com/rancher/rancher/releases/download/$1/rancher-images.txt
+wget https://github.com/rancher/rancher/releases/download/$1/rancher-load-images.sh
+wget https://github.com/rancher/rancher/releases/download/$1/rancher-save-images.sh
