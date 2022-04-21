01ed9521512f2145c20e35085ed91e362f0d8af2
Mike Russell <michael.russell@suse.com>
2022-04-21 16:37:12 -0700

    feat: WIP Adjustments
    * sets up harvester nodes patching deployments and configmaps of rke2
      coredns
    
    Resolves: feat/2096-air-gapped-rancher-single-node-proof-of-concept

--- vagrant-pxe-harvester/setup_harvester.sh
+++ vagrant-pxe-harvester/setup_harvester.sh
@@ -4,7 +4,7 @@ MYNAME=$0
 ROOTDIR=$(dirname $(readlink -e $MYNAME))
 
 pushd $ROOTDIR
-ansible-playbook ansible/setup_harvester.yml --extra-vars "@settings.yml"
+ansible-playbook ansible/setup_harvester.yml --extra-vars "@settings.yml" && ansible-playbook ansible/prepare_harvester_nodes.yml --extra-vars "@settings.yml" -i inventory
 ANSIBLE_PLAYBOOK_RESULT=$?
 popd
 exit $ANSIBLE_PLAYBOOK_RESULT
